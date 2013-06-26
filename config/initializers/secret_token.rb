e47bcad6ed7af694f53024d91052a55455a7a8c2
Dinkar Thakur <thakur.dinkar@gmail.com>
2013-06-26 16:12:36 +0530

    rail comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblabsolution::Application.config.secret_key_base = '6cb680bf3211151a27931ea65628c367a4905cc846b4b9c7a7261545d056915e72eeb859dcd6934dbf744b686c8a633ee0bd629549b026619624b7beb6fe0bbf'
